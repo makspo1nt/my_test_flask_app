20cc489042f43ba024c0f20509b26323a93aa5ff
makspo1nt <makspo1nt@yandex.ru>
2023-06-03 20:41:16 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import git
 
 app = Flask(__name__)
 
-'kek'
+'lol'
+
 
 @app.route('/')
 def index():
@@ -25,16 +26,13 @@ def api():
     return jsonify({"message": "Hello pes!"})
 
 
-@app.route('/update_server', methods=['POST', 'GET'])
+@app.route('/update_server')
 def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('/home/ekzoskelet/my_test_flask_app')
-        origin = repo.remotes.origin
-        origin.pull()
-
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
+    repo = git.Repo('./my_test_flask_app')
+    for remote in repo.remotes:
+        remote.fetch()
+    repo.git.reset('--hard', 'origin/master')
+    return 'Updated PythonAnywhere successfully', 200
 
 
 if __name__ == "__main__":
