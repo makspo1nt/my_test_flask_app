fdbbad9249762007a4c13d4ccba09539731e3dff
makspo1nt <makspo1nt@yandex.ru>
2023-06-03 19:54:55 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -23,5 +23,16 @@ def api():
     return jsonify({"message": "Hello pes!"})
 
 
+@app.route('/update_server', methods=['POST', 'GET'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/ekzoskelet/my_test_flask_app')
+        origin = repo.remotes.origin
+        origin.pull()
+
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 if __name__ == "__main__":
     app.run()
